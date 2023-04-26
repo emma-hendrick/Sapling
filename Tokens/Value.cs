0539c227430c103b93abc7453295603073ffc149
Michael Hendrick <mhendrick@byui.edu>
2023-04-26 15:20:03 -0600

    A class which represents anything from strings to booleans to lists

---
+++ Tokens/Value.cs
@@ -0,0 +1,22 @@
+namespace Sapling.Tokens;
+using Sapling.Interfaces;
+
+/// <summary>
+/// Class <c>Value</c> represents a valid value (string, bool, int, etc) within the sapling programming language.
+/// </summary>
+internal abstract class Value: Node, IToken
+{
+    /// <summary>
+    /// This construsts a new instance of a value (string, bool, int, etc).
+    /// <example>
+    /// For example:
+    /// <code>
+    /// Value myName = new Value(0, 0, "Michael");
+    /// </code>
+    /// will create a new Value instance with the value of "Michael".
+    /// </example>
+    /// </summary>
+    public Value(int lineNum, int linePos, string value): base(lineNum, linePos, value)
+    {
+    }
+}
