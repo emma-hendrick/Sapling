cfcbe6e1c419173ec314b16149511805a27ee87b
Michael Hendrick <mhendrick@byui.edu>
2023-06-17 16:20:47 -0600

    Remove append method

--- Source/Parsing/SlNodes/SlNode.cs
+++ Source/Parsing/SlNodes/SlNode.cs
@@ -5,11 +5,6 @@ namespace Sapling.Nodes;
 internal abstract class SlNode
 {
     /// <summary>
-    /// Append a node as a child of this.
-    /// <example>
-    public abstract void Append(SlNode node);
-
-    /// <summary>
     /// Generate the LLVM bitcode for this node and its children.
     /// <example>
     public abstract void GenerateCode();
