828586f0a3f7b771f8fb10a2ce844b38d0944869
Michael Hendrick <mhendrick@byui.edu>
2023-07-07 23:44:53 -0600

    Path variable

--- Source/Environment/EnvironmentLoader.cs
+++ Source/Environment/EnvironmentLoader.cs
@@ -11,10 +11,16 @@ internal class EnvironmentLoader
     private Dictionary<string, string> variables = new Dictionary<string, string>();
 
     /// <summary>
+    /// The path to the env folder
+    /// </summary>
+    private string _path;
+
+    /// <summary>
     /// Construct a new environment loader
     /// </summary>
-    public EnvironmentLoader()
+    public EnvironmentLoader(string path)
     {
+        _path = path;
         Load();
     }
 
@@ -23,12 +29,12 @@ internal class EnvironmentLoader
     /// </summary>
     private void Load()
     {
-        string[] lines = File.ReadAllLines(".env");
+        string[] lines = File.ReadAllLines(_path);
 
         foreach (string line in lines)
         {
             string[] splitLine = line.Split('=');
-            if (splitLine.Length != 2) throw new Exception($"Invalid line in .env: {line}");
+            if (splitLine.Length != 2) throw new Exception($"Invalid line in {_path}: {line}");
             variables.Add(splitLine[0], splitLine[1]);
         }
     }
